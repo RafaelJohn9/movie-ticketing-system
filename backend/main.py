a4374087dc98011666382e2fb4b9d6d451e24cc1
RafaelJohn9 <rafaeljohb@gmail.com>
2026-02-26 02:58:29 +0300

    chore (Backend): Added Frontend Link inside CORS.

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # your React dev server, e.g. Vite
+    allow_origins=["http://localhost:5173", "https://rafaeljohn9.github.io/movie-ticketing-system"],  # your React dev server, e.g. Vite
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
