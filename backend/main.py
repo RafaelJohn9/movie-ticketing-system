88de080e9be661afb88c3faa3ba539881b75db1e
RafaelJohn9 <rafaeljohb@gmail.com>
2026-03-01 13:31:09 +0300

    feat (backend): added mutcu tickets subdomain to be supported under CORS

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173",
                    "https://rafaeljohn9.github.io",
                    "https://rafaeljohn9.github.io/movie-ticketing-system",
+                   "https://tickets.mutcu.org"
                    ],
     allow_credentials=True,
     allow_methods=["*"],
